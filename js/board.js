9a164b94cafd5556528d84a6b344d8e87b92fed1
Ignacio Furió José <bichoifj@gmail.com>
2023-02-13 18:58:28 +0100

    The game is functional, i can play with only 3 tokens, next step is to check winners

--- js/board.js
+++ js/board.js
@@ -11,3 +11,49 @@ player1.innerHTML = `${name1}`
 player2.innerHTML = `${name2}`
 
 
+
+// board check 
+let board = Array.from(document.getElementsByClassName("cellDesign"));
+console.log(board)
+let turn = 1;
+
+let token = 1
+
+
+board.map(
+    (cell) => {
+        cell.addEventListener('click', () => {
+                if(cell.innerHTML === "" && turn <= 6){
+                    turn % 2 !== 0 ? cell.innerHTML = '<img src="../assets/circle.png" class="token" alt="">' : cell.innerHTML = '<img src="../assets/cross.png" class="token" alt="">';
+                    turn ++ ;           
+                }else if (cell.innerHTML === '<img src="../assets/circle.png" class="token" alt="">' && turn % 2 !== 0 && turn > 6 && token === 1){
+                    cell.innerHTML = "";
+                    token --
+                }else if (cell.innerHTML === '<img src="../assets/cross.png" class="token" alt="">' && turn % 2 === 0 && turn > 6 && token === 1){
+                    cell.innerHTML = "";
+                    token --
+                }else if (cell.innerHTML === "" && turn > 6 && token === 0){
+                    turn % 2 !== 0 ? cell.innerHTML = '<img src="../assets/circle.png" class="token" alt="">' : cell.innerHTML = '<img src="../assets/cross.png" class="token" alt="">';
+                    turn++
+                    token++
+                };
+        })
+    }
+)
+
+
+
+
+
+// function that checks if someone has won the game 
+
+// const winner = () => {
+
+//     let check = Array.from(document.getElementsByClassName("cellDesign"))
+//     switch(check){
+//         case (check[0] === check[1] && check[1] === check[2] && check[0] != ""):
+
+        
+//     };
+
+// };
