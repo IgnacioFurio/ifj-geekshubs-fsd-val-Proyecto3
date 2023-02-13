d06bfb11890931c96d3cd697fe1c76d6dd1cbc77
Ignacio Furió José <bichoifj@gmail.com>
2023-02-13 22:31:34 +0100

    Function winner is applied, trying to catch the players name that has won teh game

--- js/board.js
+++ js/board.js
@@ -12,9 +12,45 @@ player2.innerHTML = `${name2}`
 
 
 
-// board check 
+// setting board and tokens
 let board = Array.from(document.getElementsByClassName("cellDesign"));
-console.log(board)
+
+
+// current board status 
+let game = ["", "", "", "", "", "", "", "", ""]
+
+// victory array to check 
+let victory = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0 ,3 ,6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0 ,4 ,8],
+    [2 ,4 ,6],
+];
+
+// winner function 
+
+const winner = () => {
+    // we are going to check every win condition at victory array
+    for(let i = 0; i < victory.length ; i++){
+        // setting win condition value to check every loop
+        let winCondition = victory[i]
+        
+        let position0 = game[winCondition[0]];
+        let position1 = game[winCondition[1]];
+        let position2 = game[winCondition[2]];
+
+        if(position0 === position1 && position1 === position2 && position0 !== ""){
+            window.location.href = "../pages/winner.html"
+        };
+    };
+
+};
+
+// setting the basics for the game 
 let turn = 1;
 
 let token = 1
@@ -23,37 +59,44 @@ let token = 1
 board.map(
     (cell) => {
         cell.addEventListener('click', () => {
-                if(cell.innerHTML === "" && turn <= 6){
-                    turn % 2 !== 0 ? cell.innerHTML = '<img src="../assets/circle.png" class="token" alt="">' : cell.innerHTML = '<img src="../assets/cross.png" class="token" alt="">';
-                    turn ++ ;           
-                }else if (cell.innerHTML === '<img src="../assets/circle.png" class="token" alt="">' && turn % 2 !== 0 && turn > 6 && token === 1){
-                    cell.innerHTML = "";
-                    token --
-                }else if (cell.innerHTML === '<img src="../assets/cross.png" class="token" alt="">' && turn % 2 === 0 && turn > 6 && token === 1){
-                    cell.innerHTML = "";
-                    token --
-                }else if (cell.innerHTML === "" && turn > 6 && token === 0){
-                    turn % 2 !== 0 ? cell.innerHTML = '<img src="../assets/circle.png" class="token" alt="">' : cell.innerHTML = '<img src="../assets/cross.png" class="token" alt="">';
-                    turn++
-                    token++
-                };
+            // to set tokens before both playes has 3 tokens in game 
+            if(cell.innerHTML === "" && turn <= 6){
+                // to paint the cell 
+                turn % 2 !== 0 ? cell.innerHTML = '<img src="../assets/circle.png" class="token" alt="">' : cell.innerHTML = '<img src="../assets/cross.png" class="token" alt="">';
+                // to set place in the game array
+                turn % 2 !== 0 ? game[cell.id] = "O" : game[cell.id] = "X";
+
+                turn ++ ;           
+            }
+            // to retire a O token 
+            else if (cell.innerHTML === '<img src="../assets/circle.png" class="token" alt="">' && turn % 2 !== 0 && turn > 6 && token === 1){
+                // to clean the cell 
+                cell.innerHTML = "";
+                // to clean place in the array 
+                game[cell.id] = "";
+
+                token --
+            }
+            // to retire a X token 
+            else if (cell.innerHTML === '<img src="../assets/cross.png" class="token" alt="">' && turn % 2 === 0 && turn > 6 && token === 1){
+                // to clean the cell 
+                cell.innerHTML = "";
+                // to clean place in the array 
+                game[cell.id] = "";
+                token --
+            }
+            // to set again the token removed (both players)
+            else if (cell.innerHTML === "" && turn > 6 && token === 0){
+                turn % 2 !== 0 ? cell.innerHTML = '<img src="../assets/circle.png" class="token" alt="">' : cell.innerHTML = '<img src="../assets/cross.png" class="token" alt="">';
+                // to set place in the game array
+                turn % 2 !== 0 ? game[cell.id] = "O" : game[cell.id] = "X";
+                turn++
+                token++
+            };
+            winner()
         })
     }
 )
 
 
 
-
-
-// function that checks if someone has won the game 
-
-// const winner = () => {
-
-//     let check = Array.from(document.getElementsByClassName("cellDesign"))
-//     switch(check){
-//         case (check[0] === check[1] && check[1] === check[2] && check[0] != ""):
-
-        
-//     };
-
-// };
